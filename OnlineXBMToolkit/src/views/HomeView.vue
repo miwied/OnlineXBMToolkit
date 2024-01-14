d24c5f63c6010723f678a55dec4d549ae7aaa9e7
miwied <derpixed@gmail.com>
2024-01-14 19:52:07 +0100

    working on ux / ui

--- OnlineXBMToolkit/src/views/HomeView.vue
+++ OnlineXBMToolkit/src/views/HomeView.vue
@@ -1,44 +1,64 @@
 <template>
     <div class="xbm-tool-container">
-        <div class="viewer-editor-container">
-            <xbmViewer ref="xbmViewer" class="icon-container upper-icon" :xbmArray="xbmArray" :gridWidth="gridSize"
-                :gridHeight="gridSize" />
-
-            <xbmEditor ref="xbmEditor" class="icon-container" :xbmArray="xbmArray" :gridWidth="gridSize"
-                :gridHeight="gridSize" @update-array="updateArray" />
-
-            <div class="size-inputs">
-                <v-btn @click="scaleGridDown" icon="mdi-minus"></v-btn>
-                <v-text-field placeholder="Size" v-model="gridSize" center-affix></v-text-field>
-                <v-btn @click="scaleGridUp" icon="mdi-plus"></v-btn>
-            </div>
-
-            <div class="editor-buttons">
-                <v-btn @click="shiftLeft" icon="mdi-chevron-left"></v-btn>
-                <v-btn @click="shiftRight" icon="mdi-chevron-right"></v-btn>
-                <v-btn @click="shiftUp" icon="mdi-chevron-up"></v-btn>
-                <v-btn @click="shiftDown" icon="mdi-chevron-down"></v-btn>
-            </div>
-            <div class="editor-buttons">
-                <v-btn @click="rotateLeft" icon="mdi-rotate-left"></v-btn>
-                <v-btn @click="rotateRight" icon="mdi-rotate-right"></v-btn>
-            </div>
-
-            <xbmConverter ref="xbmConverter" :image="originalImage" :gridWidth="gridSize" :gridHeight="gridSize"
-                :imageWidth="imageSize" :imageHeight="imageSize" @xbm-array-converted="onConvertedXbmArray" />
-        </div>
-        <div class="settings-container">
-            <v-file-input ref="fileInput" class="file-input" v-model="originalImage" label="Upload and convert a image"
-                @change="handleFileChange" accept=".svg, .png, .jpg, .jpeg" filled clearable></v-file-input>
-            <v-btn @click="handleUpload" prepend-icon="mdi-publish"> Upload file </v-btn>
-            <div class="size-inputs">
-                <v-btn @click="scaleImageDown" icon="mdi-minus"></v-btn>
-                <v-text-field placeholder="Size" v-model="imageSize" center-affix></v-text-field>
-                <v-btn @click="scaleImageUp" icon="mdi-plus"></v-btn>
-            </div>
+        <v-card prepend-icon="mdi-publish" title="Input" class="card-container" variant="tonal">
+            <v-file-input ref="fileInput" style="opacity : 0;" v-model="originalImage" @change="handleFileChange"
+                accept=".svg, .png, .jpg, .jpeg"></v-file-input>
+            <v-btn @click="handleUpload" prepend-icon="mdi-publish">Upload and convert image</v-btn>
             <v-btn @click="reset" prepend-icon="mdi-delete"> Reset </v-btn>
             <v-btn @click="invert" prepend-icon="mdi-invert-colors"> Invert </v-btn>
+            <div class="size-inputs">
+                <v-btn @click="scaleGridDown" icon="mdi-minus"></v-btn>
+                <v-text-field label="Grid-height" placeholder="Size" v-model="gridHeight"></v-text-field>
+                <v-text-field label="Grid-width" placeholder="Size" v-model="gridWidth"></v-text-field>
+                <v-btn @click="scaleGridUp" icon="mdi-plus"></v-btn>
+            </div>
+            <div class="size-inputs">
+                <v-btn @click="scaleImageDown" icon="mdi-minus"></v-btn>
+                <v-text-field label="Image-height" placeholder="Size" v-model="imageHeight"></v-text-field>
+                <v-text-field label="Image-width" placeholder="Size" v-model="imageWidth"></v-text-field>
+                <v-btn @click="scaleImageUp" icon="mdi-plus"></v-btn>
+            </div>
+        </v-card>
+        <v-divider :thickness="6" vertical></v-divider>
+        <div class="card-container">
+            <xbmViewer ref="xbmViewer" class="xbm-tool" :xbmArray="xbmArray" :gridWidth="gridWidth"
+                :gridHeight="gridHeight" />
+            <v-card prepend-icon="mdi-eye" title="Preview" class="card-container">
+            </v-card>
+            <v-card prepend-icon="mdi-pencil" title="Edit" class="card-container">
+                <xbmEditor ref="xbmEditor" class="xbm-tool" :xbmArray="xbmArray" :gridWidth="gridWidth"
+                    :gridHeight="gridHeight" @update-array="updateArray" />
+                <div class="editor-buttons">
+                    <v-btn @click="shiftLeft" icon="mdi-chevron-left"></v-btn>
+                    <v-btn @click="shiftRight" icon="mdi-chevron-right"></v-btn>
+                    <v-btn @click="shiftUp" icon="mdi-chevron-up"></v-btn>
+                    <v-btn @click="shiftDown" icon="mdi-chevron-down"></v-btn>
+                </div>
+                <div class="editor-buttons">
+                    <v-btn @click="rotateLeft" icon="mdi-rotate-left"></v-btn>
+                    <v-btn @click="rotateRight" icon="mdi-rotate-right"></v-btn>
+                </div>
+
+                <xbmConverter ref="xbmConverter" :image="originalImage" :gridWidth="gridWidth" :gridHeight="gridHeight"
+                    :imageWidth="imageWidth" :imageHeight="imageHeight" @xbm-array-converted="onConvertedXbmArray" />
+            </v-card>
         </div>
+        <v-divider :thickness="6" vertical></v-divider>
+
+        <v-card prepend-icon="mdi-download" title="Output" class="card-container">
+            <v-container>
+                <v-row>
+                    <v-col cols="12" sm="9">
+                        <v-text-field v-model="text" label="Copy this text" readonly></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="3">
+                        <v-btn @click="copyText">
+                            Copy
+                        </v-btn>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
     </div>
 </template>
   
@@ -58,8 +78,10 @@ export default {
             xbmArray:
                 [],
             originalImage: '',
-            gridSize: 24,
-            imageSize: 24,
+            gridHeight: 24,
+            gridWidth: 24,
+            imageHeight: 24,
+            imageWidth: 24,
         };
     },
     methods: {
@@ -74,6 +96,7 @@ export default {
                 reader.onload = (e) => {
                     this.originalImage = e.target.result;
                     const img = new Image();
+                    console.log("test");
                     img.onload = () => {
                         this.$refs.xbmConverter.convertToXbm();
                     };
@@ -119,18 +142,33 @@ export default {
             this.$refs.xbmEditor.rotateRight()();
         },
         scaleGridUp() {
-            this.gridSize = this.gridSize + 1;
+            this.gridWidth++;
+            this.gridHeight++;
+            if (this.originalImage != null && this.originalImage != undefined) {
+                // this.$refs.xbmConverter.convertToXbm();
+            }
         },
         scaleGridDown() {
-            this.gridSize = this.gridSize - 1;
+            this.gridWidth--;
+            this.gridHeight--;
+            if (this.originalImage !== null && this.originalImage !== undefined) {
+                // this.$refs.xbmConverter.convertToXbm();
+            }
         },
         scaleImageUp() {
+            this.imageWidth++;
+            this.imageHeight++;
             this.imageSize = this.imageSize + 1;
-            this.$refs.xbmConverter.convertToXbm();
+            if (this.originalImage !== null && this.originalImage !== undefined) {
+                this.$refs.xbmConverter.convertToXbm();
+            }
         },
         scaleImageDown() {
-            this.imageSize = this.imageSize - 1;
-            this.$refs.xbmConverter.convertToXbm();
+            this.imageWidth--;
+            this.imageHeight--;
+            if (this.originalImage !== null && this.originalImage !== undefined) {
+                this.$refs.xbmConverter.convertToXbm();
+            }
         },
     }
 }
@@ -143,9 +181,17 @@ export default {
     flex-direction: row;
 }
 
+.xbm-tool {
+    width: 400px;
+}
+
 .size-inputs {
     display: flex;
     justify-content: center;
+}
+
+.size-inputs>* {
+    margin: 0.5rem;
 }
 
 .editor-buttons {
@@ -161,13 +207,18 @@ export default {
     justify-content: center;
 }
 
-.settings-container {
-    width: 30%;
+.card-container {
+    margin: 0.5rem;
+    padding: 0.5rem;
+    width: 100%;
+}
+
+.card-container {
     display: flex;
     flex-direction: column;
 }
 
-.settings-container>* {
+.card-container>* {
     margin-top: 0.5rem;
     margin-bottom: 0.5rem;
 }
